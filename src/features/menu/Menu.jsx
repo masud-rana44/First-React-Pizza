52a280a14392f8f742d5c54a83ddbf7c349e3a42
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-03 01:32:15 +0600

    fetched menu data from api using render as you fetch approach

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,5 +1,23 @@
+import { useLoaderData } from "react-router-dom";
+import { getMenu } from "../../services/apiRestaurant.js";
+import MenuItem from "./MenuItem";
+
 function Menu() {
-  return <h1>Menu</h1>;
+  // Here use render as you fetch strategy, render the component and fetch the data simultaneously, no data loading waterfalls like fetch on render approach (before render, then fetch)
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((pizza) => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
 }
 
 export default Menu;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <header>
+      <Link to="/">First React Pizza Co.</Link>
+
+      <p>Masud</p>
+    </header>
+  );
+}
+
+export default Header;
