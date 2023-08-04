ceaa196c681250f180bee793b494dcfde3f7a97d
masud-rana44 <mdmasudrana4488@gmail.com>
2023-08-04 12:12:36 +0600

    setup redux toolkit and created userslice

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,8 @@
 import { Form, redirect, useActionData, useNavigation } from 'react-router-dom';
 import { createOrder } from '../../services/apiRestaurant';
 import Button from '../../ui/Button';
+import { useSelector } from 'react-redux';
+import { getUsername } from '../user/userSlice';
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -37,6 +39,7 @@ function CreateOrder() {
   const isSubmitting = navigation.state === 'submitting';
 
   const formErrors = useActionData();
+  const username = useSelector(getUsername);
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
@@ -51,7 +54,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            name="customer"
+            defaultValue={username}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- src/features/user/Username.jsx
+++ src/features/user/Username.jsx
@@ -1,5 +1,12 @@
+import { useSelector } from 'react-redux';
+import { getUsername } from './userSlice';
+
 function Username() {
-  return <p className="hidden text-sm font-semibold md:block">Masud</p>;
+  const username = useSelector(getUsername);
+
+  if (!username) return null;
+
+  return <p className="hidden text-sm font-semibold md:block">{username}</p>;
 }
 
 export default Username;
